17847e65bca1843263d09bb7e685e81596bcd38c
MarounnGhayaa <marounghaya@icloud.com>
2025-06-30 14:39:21 +0300

    Added profile functionality

---
+++ scripts/profile.js
@@ -0,0 +1,8 @@
+const storedUser = localStorage.getItem('currentUser');
+if (storedUser) {
+    document.getElementById('user-name').textContent = storedUser.name;
+    document.getElementById('user-email').textContent = storedUser.email;
+    document.getElementById('user-phone').textContent = storedUser.phone_number;
+    document.getElementById('user-favorite-genre').textContent = storedUser.favorite_genre;
+    document.getElementById('user-age').textContent = storedUser.age;
+}
