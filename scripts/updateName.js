9b2caf4102e28d532fdc0561a302b74a6e05c025
MarounnGhayaa <marounghaya@icloud.com>
2025-06-30 14:43:44 +0300

    Added the update name option functionality

---
+++ scripts/updateName.js
@@ -0,0 +1,25 @@
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+const updateNameButton = document.getElementById('editName');
+updateNameButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    const newName = document.getElementById('newNameInput').value;
+
+    axios.post("http://localhost/cinema-server/controllers/update_name.php", {
+        column_name: "name",
+        new_value: newName,
+        id: currentUser.id
+    })
+    .then(response => {
+        if (response.data.success) {
+            console.log("Name updated:", response.data.updatedUser);
+            alert("Name updated successfully!");
+            document.getElementById('user-name').innerText = newName;
+        } else {
+            alert("Update failed: " + response.data.message);
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("An error occurred while updating.");
+    });
+});
