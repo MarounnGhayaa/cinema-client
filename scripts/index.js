516f79074a545cfaf1c00100fcfb8c562695eaef
MarounnGhayaa <marounghaya@icloud.com>
2025-06-30 14:30:51 +0300

    Added login/signup functionality

---
+++ scripts/index.js
@@ -0,0 +1,83 @@
+const showRegisterLink = document.getElementById('showRegister');
+const showLoginLink = document.getElementById('showLogin');
+const loginField = document.getElementById('loginForm');
+const registerField = document.getElementById('registerForm');
+
+showRegisterLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    loginField.classList.add('hidden');
+    registerField.classList.remove('hidden');
+});
+
+showLoginLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    registerField.classList.add('hidden');
+    loginField.classList.remove('hidden');
+});
+
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('log-email').value;
+    const password = document.getElementById('log-pass').value;
+
+    axios.post('https://localhost/cinema-server/controllers/login.php', {
+        email: email,
+        password: password
+    })
+    .then(response => {
+        console.log(response.data);
+        if (response.data.success) {
+            const user = response.data.user;
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            alert("Login successful. Welcome " + user.name);
+            if (user.role === 'admin') {
+                window.location.href = '../pages/adminPage.html';
+            } else {
+                window.location.href = '../pages/userPage.html';
+            }
+        } else {
+            alert("Login failed: " + response.data.message);
+        }
+    })
+    .catch(error => {
+        console.error("API error:", error);
+    });
+});
+
+const registerForm = document.getElementById('register-form');
+
+registerForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('regName').value;
+    const email = document.getElementById('regEmail').value;
+    const password = document.getElementById('regPass').value;
+    const phone_number = document.getElementById('regPhone').value;
+    const age = document.getElementById('regAge').value;
+    const favorite_genre = document.getElementById('regFavGenre').value;
+
+    axios.post('https://localhost/cinema-server/controllers/register.php', {
+        name: name,
+        email: email,
+        password: password,
+        phone_number: phone_number,
+        age: age,
+        favorite_genre: favorite_genre
+    })
+    .then(response => {
+        console.log(response.data);
+        if (response.data.success) {
+            const user = response.data.user;
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            alert("Registration successful. Welcome " + user.name);
+            window.location.href = '../pages/userPage.html';
+        } else {
+            alert("Registration failed: " + response.data.message);
+        }
+    })
+    .catch(error => {
+        console.error("API error:", error);
+    });
+});
